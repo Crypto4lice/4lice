9acebeb87870a18638125eb24ebbebdcd0f4663a
bap9jo <bap9jo@gmail.com>
2017-07-28 19:27:58 +0900

    Project-3 submit

--- Project-3/Project-3.cpp
+++ Project-3/Project-3.cpp
@@ -6,20 +6,21 @@
 using namespace std;
 
 void main() {
-	int max_line;
-	int player = 0;
-	int squirrel = 0;
-	int rand_row;
-	int rand_col;
-	string buf;
-	srand((unsigned int)time(NULL));
-	vector<vector <char>> line;
+	int max_line;		// field size var
+	int player = 0;		// count player var
+	int squirrel = 0;	// count squirrel var
+	int rand_row;		// squirrel point x 
+	int rand_col;		// squirrel point y
+	string buf;			// read line
+	srand((unsigned int)time(NULL));	// set squirrel point x,y as random seed = time
+	vector<vector <char>> line;			// field data
 	cin >> max_line;
-	line.assign(max_line, vector<char>(max_line, ' '));
+	line.assign(max_line, vector<char>(max_line, ' '));	// set the field size and initialize
+	// set field data
 	for (int i = 0; i < max_line; i++) {
 		cin >> buf;
 		for (int j = 0; j < max_line; j++) {
-			line[i][j] = buf[j];
+			line[i][j] = buf[j];	
 			if (line[i][j] == 'C')
 				player++;
 		}
@@ -28,18 +29,21 @@ void main() {
 
 	
 	cout << "----------------" << endl;
-
+	// make a squirrel 
 	while (squirrel < 2 * player) {
-		rand_row = rand() % 5;
+		// set squirrel position random
+		rand_row = rand() % 5;	
 		rand_col = rand() % 5;
+		// if player already exist set another position
 		if (line[rand_row][rand_col] == 'C')
 			continue;
+		// if player does not exist that position spawn squirrel
 		if (line[rand_row][rand_col] == '.') {
 			line[rand_row][rand_col] = 'D';
 			squirrel++;
 		}
 	}
-
+	// print refreshed field
 	for each (auto var in line) {
 		for each(auto var2 in var) {
 			cout << var2;
