22844388045af08acd596180bfd9c33ad30f9ca0
bap9jo <bap9jo@gmail.com>
2017-08-03 03:51:39 +0900

    Project-4 submit

--- Project-4/resource.py
+++ Project-4/resource.py
@@ -1,8 +1,11 @@
+# varlue for listmap
 resource = []
+# open file and listmap's key
 rsc = open("./resource.txt", "r")
 readhead = rsc.readline()
 readhead = readhead.split(' ')
 
+# set listmap
 for line in rsc:
     splitine = line.split(',')
     dict = {}
@@ -11,19 +14,25 @@ for line in rsc:
     dict[readhead[2]] = splitine[2]
     dict[readhead[3]] = splitine[3]
     resource.append(dict)
-
+# input users command
 com = input("Enter a search string : ")
 find = 0
 print("Results : ")
 print("Name                  | Position          | Separation Date")
 print("-----------------------------------------------------------")
+# searching containing certain word data
 for item in resource:
+    # listmap -> map -> values
     for var in item.values():
         for i in range(len(var)):
-           if com in var:
-               print(item.get("FirstName").ljust(10), item.get("LastName").ljust(12), item.get("Position").ljust(20), item.get("SeparationDate\n"))
-               find = 1
-               break
+            # compare certain word
+            if com in var:
+                # if containing print listmap
+                print(item.get("FirstName").ljust(10), item.get("LastName").ljust(12), item.get("Position").ljust(20), item.get("SeparationDate\n"))
+                # add find flag to avoid overlap
+                find = 1
+                break
+        # if overlap occur -> break out 
         if find == 1 :
             break
     find = 0
